0ce183d55b7f4de9a3d5af127fea1ba24418af30
Stuart Weinstein <stuart.weinstein@student.fairfield.edu>
2018-12-05 03:34:04 +0000

    added mod df function

--- Stuart/.ipynb_checkpoints/back_end-checkpoint.py
+++ Stuart/.ipynb_checkpoints/back_end-checkpoint.py
@@ -1,81 +1,111 @@
-#KJASHDG test
+
 #imports that we need
 import requests
 import json
 import csv
 import pandas as pd
 import os.path
+import numpy as np
+import matplotlib.pyplot as plt
 
-#API_KEY  !DO NOT TOUCH!
+
+#API_KEY FOR ALPHAVANTAGE API !DO NOT TOUCH!
 API_KEY = '4HNDQOUQ2A1G90RW'
 
+#api link for later if we want to use [news and things like that]
+#api_url = https://api.iextrading.com/1.0/stock/aapl/batch?types=quote,news,chart&range=1m&last=10
+
+#api link for stock twits and sentiment analysis if we want
+#https://api.stocktwits.com/api/2/streams/symbol/AAPL.json
 
 
-#get the JSON values for the entred symbols
-def alphaVantageJSON(symbol):
 
-    # Use this API when calling the full data files
-    #url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + symbol + '&outputsize=full&apikey=' + API_KEY
+'''
+#this is a test function to see if we can streamline code. Dont touch/worry about this right now
+def pass_symbol(symbol_passed):
+    global symbol
+    symbol = symbol_passed
+    return
+'''
+
+#makes a list of inuts for the user to enter to  
+def get_stocks():
     
-    #generates the URL based on the symbol imported and gets the JSON data from the API. 
-    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + symbol + '&outputsize=compact&apikey=' + API_KEY
-    
-    
-    alphaVantage = requests.get(url)
-    alphaVantage_json_str = alphaVantage.content
-    alphaVantage_data = json.loads(alphaVantage_json_str)
-    
-   
-    #gets all the 'days' as the keys from the JSON 
-    days = alphaVantage_data['Time Series (Daily)'].keys()
-    
-    #get the open price for every day
-    for k in days:
-        date = str(k)
-        open_price = alphaVantage_data['Time Series (Daily)'][date]['1. open']
-        open_price_float = float(open_price)
+    stock_symbols = []
+    while True:
+        symbol = input("Enter a stock symbol: ").upper()
         
-        
-        #works but converts them to a string for pretty output
-        open_price_round = '${:,.2f}'.format(open_price_float)
-        
-        print(k + " Open Price: " + open_price_round)
+        if len(stock_symbols) >= 4 or symbol.lower() == "no":
+            break
+        else:
+            stock_symbols.append(symbol)
+    return(stock_symbols)
+
+#this function makes the dataframe from the CSV given from teh API. It cleans up the rows and sets the index column to the dates
+def make_df(symbol):
     
+    #Dont use this one for testing, only deployemnt
+    #url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + symbol_save + '&outputsize=full&datatype=csv&apikey=' + API_KEY
+
+    #generates the URL based on the symbol imported and gets the JSON data from the API if file hasn't been written before
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + symbol + '&outputsize=compact&datatype=csv&apikey=' + API_KEY
+    data = pd.read_csv(url, index_col="timestamp", parse_dates = True, na_values = ' ')
+    data.index.names = ['date']
+    print("DF Created!")
+    return (data)
+
+
+#adding oc_var, volatility, fluctuation, MA(5), MA(30) & MA(252)
+def mod_df(df):
+    df['fluctuation'] = 100*(df['high']-df['low'])/df['close']
+    df['oc_var'] = df['open'] - df['close']
+    df['volatility'] = df['high'] - df['low']
+    
+    return (df)
+
+
+
+
+
+#makes a summary of todays stock information
+def todays_summary(df):
+    open_price = '${:,.2f}'.format(df.iloc[0]['open'])
+    high_price = '${:,.2f}'.format(df.iloc[0]['high'])
+    low_price = '${:,.2f}'.format(df.iloc[0]['low'])
+    close_price = '${:,.2f}'.format(df.iloc[0]['close'])
+    volume = "{:,}".format(int(df.iloc[0]['volume']))
+    fluctuation = '{:.2f}'.format(df.iloc[0]['fluctuation'])
+    
+    print("Todays Opening Price: ", open_price)
+    print("Todays Highest Price: ", high_price)
+    print("Todays Lowest Price: ", low_price)
+    print("Todays Closing Price: ", close_price)
+    print("Todays Trading Volume: ", volume)
+    print("Todays Price Fluctuation: ", fluctuation,"%")
     return
 
 
-
-#get the CSV values for the entred symbols from the API
-def alphaVantageCSV(symbol):
-
-    csv_name = 'csv_files/' + symbol.lower() + "_data.csv"
-    
-    
-    #write new CSV file
-    if os.path.isfile(csv_name) != True:
-    
-        #Dont use this one for testing, only deployemnt
-        #url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + symbol + '&outputsize=full&datatype=csv&apikey=' + API_KEY
-        
-        
-        #generates the URL based on the symbol imported and gets the JSON data from the API if file hasn't been written before
-        url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + symbol + '&outputsize=compact&datatype=csv&apikey=' + API_KEY
-        
-        
-        data = pd.read_csv(url)
-        csv_name = 'csv_files/' + symbol.lower() + "_data.csv"
-        data.to_csv(csv_name)
-        print("Writing New File... One Moment")
-        
-    #update current CSV file
-    else:
-        print("Updating current file... One Moment")
-        url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + symbol + '&outputsize=compact&datatype=csv&apikey=' + API_KEY
-        data = pd.read_csv(url)
-        csv_name = 'csv_files/' + symbol.lower() + "_data.csv"
-        data.to_csv(csv_name)
-    
+#diaplsy the open and close price vs date in a graph 
+def graph(df,symbol):
+    df['close'].plot(color ='r', label = symbol)
+    df['open'].plot(color ='b', label = symbol)
+    plt.legend()
+    plt.show()
     return
 
 
-
+#gets todays news articles for the stock 
+def news(symbol):
+    
+    url = 'https://api.iextrading.com/1.0/stock/' + symbol.lower() + '/batch?types=news&last=5'
+    news = requests.get(url)
+    news_json_str = news.content
+    news_data = json.loads(news_json_str)
+    
+    print('\033[1m' + "Todays News", '\n')
+    for x in range(0,5):
+        news_headline = news_data['news'][x]['headline']
+        news_url = news_data['news'][x]['url']
+        output = str(x) + ") " + news_headline + " [URL:" + news_url + "]"
+        print('\033[0m' + output)
+    return
